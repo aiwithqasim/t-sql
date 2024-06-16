6b5b24bddbe567ffb0d121d2409c02f7aacd6011
aiwithqasim <qasimhassan1020@gmail.com>
2024-06-16 06:51:33 +0500

    class01 creating & loaidng data in database

--- class01 Installation, Creation & Loading data/class01 Sql Basis part1.sql
+++
@@ -1,6 +0,0 @@
-use BikeStores;
-
--- SELECT CLAUSE
-select * from sales.customers;
-select first_name, last_name, state from sales.customers;
-select first_name, last_name, state from sales.customers where state='CA';
